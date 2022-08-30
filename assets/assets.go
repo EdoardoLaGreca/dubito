961c861b2538037d600d1f43fadb63ea3dda89a3
Edoardo La Greca <elagreca@protonmail.com>
2022-08-30 17:18:17 +0200

    add getCardAsset and getDeckAsset

--- assets/assets.go
+++ assets/assets.go
@@ -1,6 +1,70 @@
 package assets
 
-import "embed"
+import (
+	"bytes"
+	"embed"
+	"image"
+	"image/png"
+	"strconv"
+
+	"github.com/EdoardoLaGreca/dubito/internal/cardutils"
+)
 
 //go:embed cards decks
 var assets embed.FS
+
+func getCardAsset(c cardutils.Card) (image.Image, error) {
+	filename := "card_"
+
+	switch c.Suit {
+	case cardutils.Clubs:
+		filename += "c"
+	case cardutils.Diamonds:
+		filename += "d"
+	case cardutils.Hearts:
+		filename += "h"
+	case cardutils.Spades:
+		filename += "s"
+	}
+
+	switch c.Rank {
+	case cardutils.Ace:
+		filename += "a"
+	case cardutils.Jack:
+		filename += "j"
+	case cardutils.Queen:
+		filename += "q"
+	case cardutils.King:
+		filename += "k"
+	default:
+		filename += strconv.Itoa(int(c.Rank))
+	}
+
+	filename += ".png"
+
+	content, err := assets.ReadFile("cards/" + filename)
+	if err != nil {
+		return nil, err
+	}
+
+	img, err := png.Decode(bytes.NewBuffer(content))
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+func getDeckAsset(style int) (image.Image, error) {
+	content, err := assets.ReadFile("decks/deck_" + strconv.Itoa(style) + ".png")
+	if err != nil {
+		return nil, err
+	}
+
+	img, err := png.Decode(bytes.NewBuffer(content))
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
