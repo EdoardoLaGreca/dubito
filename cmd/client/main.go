03fab8a2d36ffbc9ef7b5d12c3b4f331c9754741
Edoardo La Greca <elagreca@protonmail.com>
2022-08-30 11:55:39 +0200

    move card-related things from main.go to utils.go and refactor to make them public

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,105 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"fyne.io/fyne/v2/app"
 )
-
-// suits
-type suit int
-
-const (
-	clubs suit = iota
-	diamonds
-	hearts
-	spades
-)
-
-// ranks
-type rank int
-
-const (
-	ace rank = iota
-	two
-	three
-	four
-	five
-	six
-	seven
-	eight
-	nine
-	ten
-	jack
-	queen
-	king
-)
-
-type card struct {
-	suit suit
-	rank rank
-}
 
 var username string = "Player"
 var serverAddress string = ""
 var serverPort uint16 = 9876
-
-// e.g. "five clubs" or "queen spades"
-func cardByName(name string) (card, error) {
-	nameSp := strings.Fields(name)
-
-	rankStr := nameSp[0]
-	suitStr := nameSp[1]
-
-	var c card
-
-	switch rankStr {
-	case "ace":
-		c.rank = ace
-	case "two":
-		c.rank = two
-	case "three":
-		c.rank = three
-	case "four":
-		c.rank = four
-	case "five":
-		c.rank = five
-	case "six":
-		c.rank = six
-	case "seven":
-		c.rank = seven
-	case "eight":
-		c.rank = eight
-	case "nine":
-		c.rank = nine
-	case "ten":
-		c.rank = ten
-	case "jack":
-		c.rank = jack
-	case "queen":
-		c.rank = queen
-	case "king":
-		c.rank = king
-	default:
-		return card{}, fmt.Errorf("unknown rank in " + name)
-	}
-
-	switch suitStr {
-	case "clubs":
-		c.suit = clubs
-	case "diamonds":
-		c.suit = diamonds
-	case "hearts":
-		c.suit = hearts
-	case "spades":
-		c.suit = spades
-	default:
-		return card{}, fmt.Errorf("unknown suit in " + name)
-	}
-
-	return c, nil
-}
 
 func main() {
 	a := app.New()
---
+++ cmd/client/utils.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Card struct {
+	Suit Suit
+	Rank Rank
+}
+
+// suits
+type Suit int
+
+const (
+	Clubs Suit = iota
+	Diamonds
+	Hearts
+	Spades
+)
+
+// ranks
+type Rank int
+
+const (
+	Ace Rank = 1
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+// e.g. "five clubs" or "queen spades"
+func cardByName(name string) (Card, error) {
+	nameSp := strings.Fields(name)
+
+	rankStr := nameSp[0]
+	suitStr := nameSp[1]
+
+	var c Card
+
+	switch rankStr {
+	case "ace":
+		c.Rank = Ace
+	case "two":
+		c.Rank = Two
+	case "three":
+		c.Rank = Three
+	case "four":
+		c.Rank = Four
+	case "five":
+		c.Rank = Five
+	case "six":
+		c.Rank = Six
+	case "seven":
+		c.Rank = Seven
+	case "eight":
+		c.Rank = Eight
+	case "nine":
+		c.Rank = Nine
+	case "ten":
+		c.Rank = Ten
+	case "jack":
+		c.Rank = Jack
+	case "queen":
+		c.Rank = Queen
+	case "king":
+		c.Rank = King
+	default:
+		return Card{}, fmt.Errorf("unknown rank in " + name)
+	}
+
+	switch suitStr {
+	case "clubs":
+		c.Suit = Clubs
+	case "diamonds":
+		c.Suit = Diamonds
+	case "hearts":
+		c.Suit = Hearts
+	case "spades":
+		c.Suit = Spades
+	default:
+		return Card{}, fmt.Errorf("unknown suit in " + name)
+	}
+
+	return c, nil
+}
