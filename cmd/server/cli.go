6ab09491b56a43509282104ec24302f6ce7ac811
Edoardo La Greca <elagreca@protonmail.com>
2022-09-04 13:55:28 +0200

    add getArgPos

--- cmd/server/cli.go
+++ cmd/server/cli.go
@@ -4,18 +4,15 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
-func getArgMaxPlayers() (int, error) {
-	for i, arg := range os.Args {
-		if arg == "-m" || (arg[0] == '-' && arg[1] != '-' && strings.Contains(arg, "m")) {
-			maxPlayers, err := strconv.Atoi(os.Args[i+1])
-			if err != nil {
-				return 0, err
-			}
+// return the specified argument position, -1 if it could not be found
+func getArgPos(argname string) (pos int) {
+	pos = -1
 
-			return maxPlayers, nil
+	for i, a := range os.Args {
+		if a == argname {
+			pos = i
 		}
 	}
 
