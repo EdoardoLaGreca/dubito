f3f7f0df7bc8ed294b32481e73a2b0eaaa514b87
Edoardo La Greca <elagreca@protonmail.com>
2022-09-04 13:57:07 +0200

    shrink getArgMaxPlayers and add getListenAddress and getListenPort

--- cmd/server/cli.go
+++ cmd/server/cli.go
@@ -16,5 +16,45 @@ func getArgPos(argname string) (pos int) {
 		}
 	}
 
-	return 0, fmt.Errorf("no -m arg found")
+	return
+}
+
+func getArgMaxPlayers() (int, error) {
+	pos := getArgPos("-m")
+
+	if pos == -1 {
+		return 0, fmt.Errorf("no -m arg found")
+	}
+
+	maxPlayers, err := strconv.Atoi(os.Args[pos+1])
+	if err != nil {
+		return 0, err
+	}
+
+	return maxPlayers, nil
+}
+
+func getListenAddress() (string, error) {
+	pos := getArgPos("-a")
+
+	if pos == -1 {
+		return "", fmt.Errorf("no -a arg found")
+	}
+
+	return os.Args[pos+1], nil
+}
+
+func getListenPort() (uint16, error) {
+	pos := getArgPos("-p")
+
+	if pos == -1 {
+		return 0, fmt.Errorf("no -p arg found")
+	}
+
+	port, err := strconv.Atoi(os.Args[pos+1])
+	if err != nil {
+		return 0, err
+	}
+
+	return uint16(port), nil
 }
