123b6f26192a963225fc0b899ed9f7a627449652
Edoardo La Greca <elagreca@protonmail.com>
2022-09-07 11:16:48 +0200

    add window resize

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
@@ -9,6 +10,7 @@ var username string = "Player"
 func main() {
 	a := app.New()
 	w := a.NewWindow("Dubito")
+	w.Resize(fyne.NewSize(450, 200))
 
 	menuContainer := getMenuContainer(w)
 
