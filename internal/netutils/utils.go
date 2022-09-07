e60936a8922a9ab8b0cb5ff79df27a79bdcf7ca7
Edoardo La Greca <elagreca@protonmail.com>
2022-09-07 13:58:41 +0200

    fix RecvMsg

--- internal/netutils/utils.go
+++ internal/netutils/utils.go
@@ -15,46 +15,49 @@ func SendMsg(conn net.Conn, msg string) error {
 // RecvMsg reads the connection, stores all the incoming messages in the queue as
 // successive items and returns the next item in the queue as a string
 func RecvMsg(conn net.Conn) (string, error) {
-	msg := make([]byte, 0)
-	buf := make([]byte, 1024)
+	if nq.IsEmpty() {
+		msg := make([]byte, 0)
+		buf := make([]byte, 1024)
 
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			return "", nil
-		}
-
-		// beginning of a new message
-		start := 0
-
-		for i := 0; i < n; i++ {
-			if buf[i] == 0x00 {
-				// append the new message to msg
-				msg = append(msg, buf[start:i]...)
-
-				// make a copy of the message
-				msgCopy := make([]byte, len(msg))
-				copy(msgCopy, msg)
-
-				// add the message copy to the queue
-				nqi := NewItem(msgCopy)
-				nq.AddItem(nqi)
-
-				// set/reset the variables
-				msg = make([]byte, 0)
-				start = i
-			} else if i == n-1 {
-				// append all the buffer from start
-				msg = append(msg, buf[start:]...)
-
-				// reset start
-				start = 0
+		for {
+			n, err := conn.Read(buf)
+			if err != nil {
+				return "", nil
 			}
-		}
 
-		// we reached the end of available data
-		if n < len(buf) {
-			break
+			// beginning of a new message
+			start := 0
+
+			// read all incoming messages
+			for i := 0; i < n; i++ {
+				if buf[i] == 0x00 {
+					// append the new message to msg
+					msg = append(msg, buf[start:i]...)
+
+					// make a copy of the message
+					msgCopy := make([]byte, len(msg))
+					copy(msgCopy, msg)
+
+					// add the message copy to the queue
+					nqi := NewItem(msgCopy)
+					nq.AddItem(nqi)
+
+					// set/reset the variables
+					msg = make([]byte, 0)
+					start = i + 1
+				} else if i == n-1 {
+					// append all the buffer from start
+					msg = append(msg, buf[start:]...)
+
+					// reset start
+					start = 0
+				}
+			}
+
+			// we reached the end of available data
+			if n < len(buf) {
+				break
+			}
 		}
 	}
 
